4f4f5605a9fca55b58b213c38dee1ea3d19165a1
LaurenceLungo <pnng@connect.ust.hk>
2020-04-12 02:27:44 +0800

    2-opt algo

---
+++ algo/two_opt.py
@@ -0,0 +1,41 @@
+from .plotter.plotter import Plotter
+from random import seed, randrange
+import time
+
+
+def two_opt(euclideanMap, node, init_length, route, duration):
+    st = time.time()
+    seed()
+    route_len = len(route)-1
+    global_route = route[:route_len].copy()
+    global_length = init_length
+
+    def randpos():
+        return randrange(route_len)
+
+    def cal_length(r):
+        c = 0
+        for i in range(len(r)-1):
+            j = i+1
+            c += euclideanMap[r[i]][r[j]]
+        c += euclideanMap[r[0]][r[-1]]
+        return c
+    T = cal_length(global_route) == init_length
+    timeout = time.time() + duration
+    while time.time() < timeout:
+        curr_route = global_route.copy()
+        i = randpos()
+        j = randpos()
+        temp = curr_route[i]
+        curr_route[i] = curr_route[j]
+        curr_route[j] = temp
+        curr_length = cal_length(curr_route)
+        # Plotter(node, curr_length, curr_route, True)
+        if curr_length < global_length:
+            global_route = curr_route
+            global_length = curr_length
+            print(str(global_length) + " time: " + str(time.time()-st))
+    global_route.append(global_route[0])
+    Plotter(node, global_length, global_route, False)
+    return global_length, global_route
+
