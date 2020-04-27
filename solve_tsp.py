c61bd04be1aa69a188e385e30edb0a0a6dfcc5bd
LaurenceLungo <pnng@connect.ust.hk>
2020-04-28 00:28:42 +0800

    add manual algorithm selection mode and loop mode; print out runtime and result

--- solve_tsp.py
+++ solve_tsp.py
@@ -37,6 +37,10 @@ parser.add_argument('output', metavar='o', nargs='?', default='output-tour.txt',
                     help='the path to output tour file')
 parser.add_argument('--time', metavar='-t', type=int, nargs='?', default=180,
                     help='the maximum number of seconds that the program should run')
+parser.add_argument('--algo', metavar='-a', type=int, nargs='?', default=0,
+                    help='manually select algorithm: 1 - sa; 2 - dcp - 2opt; 3 - 2-opt')
+parser.add_argument('--loop', metavar='-l', type=int, nargs='?', default=1,
+                    help='loop the program for a given time')
 parser.add_argument('-v', action='store_true',
                     help='visualize the result at the end')
 args = parser.parse_args()
@@ -79,39 +83,49 @@ for n in node:
 # end of data & arguments parsing -------------------------------------------------
 
 # main <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>><><><><><><><><><><><><><>
-deadline = time() + args_value["time"]
-final_length = None
-final_route = None
+for _ in range(args_value["loop"]):
+    deadline = time() + args_value["time"]
+    final_length = None
+    final_route = None
 
-g_length, g_route = greedy_rotate(euclidean_map)
+    g_length, g_route = greedy_rotate(euclidean_map)
+    print("g_length:", g_length, "time cost:", time() - ini_time)
 
-if len(node) <= 50:
-    best_sa_length = None
-    best_sa_route = None
-    for _ in range(int(args_value["time"] / 30 - 1)):
-        # worst case 30 sec per run
-        sa_length, sa_route = sim_annealing(euclidean_map, g_length, g_route, 2000000)
-        if best_sa_length is None or sa_length < best_sa_length:
-            best_sa_length = sa_length
-            best_sa_route = sa_route
-    final_length = best_sa_length
-    final_route = best_sa_route
-elif len(node) <= 500:
-    c_length, c_route = cross_path_dismantling(euclidean_map, node, g_route)
-    t_length, t_route = two_opt(euclidean_map, c_length, c_route, deadline)
-    final_length = t_length
-    final_route = t_route
-else:
-    t_length, t_route = two_opt(euclidean_map, g_length, g_route, deadline)
-    final_length = t_length
-    final_route = t_route
+    if args_value["algo"] == 1 or (args_value["algo"] == 0 and len(node) <= 50):
+        best_sa_length = None
+        best_sa_route = None
+        for _ in range(int(args_value["time"] / 30 - 1)):
+            # worst case 30 sec per run
+            sa_length, sa_route = sim_annealing(euclidean_map, g_length, g_route, 2000000)
+            if best_sa_length is None or sa_length < best_sa_length:
+                best_sa_length = sa_length
+                best_sa_route = sa_route
+        final_length = best_sa_length
+        final_route = best_sa_route
+    elif args_value["algo"] == 2 or (args_value["algo"] == 0 and len(node) <= 500):
+        c_length, c_route = cross_path_dismantling(euclidean_map, node, g_route)
+        print("c_length:", c_length, "time cost:", time() - ini_time)
+        t_length, t_route = two_opt(euclidean_map, c_length, c_route, deadline)
+        print("t_length:", t_length, "time cost:", time() - ini_time)
+        final_length = t_length
+        final_route = t_route
+    else:
+        t_length, t_route = two_opt(euclidean_map, g_length, g_route, deadline)
+        print("t_length:", t_length, "time cost:", time() - ini_time)
+        final_length = t_length
+        final_route = t_route
 
-output_string = "tour cost: " + str(final_length) + "\n" + "tour: " + str(final_route)
-out = open(args_value['output'], 'w')
-out.write(output_string)
-out.close()
-print("finish")
+    output_string = "tour cost: " + str(final_length) + "\n" + "tour: " + str(final_route) + "\n"
+    mode = 'w'
+    if args_value["loop"] > 1:
+        mode = 'a+'
+    out = open(args_value['output'], mode)
+    out.write(output_string)
+    out.close()
+    print(output_string)
+    print("finish")
+    print("time cost:", time() - ini_time)
 
-if args.v:
-    Visualizer(node, final_length, final_route)
+    if _ == args_value["loop"] - 1 and args.v:
+        Visualizer(node, final_length, final_route)
 # end of main <><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>
